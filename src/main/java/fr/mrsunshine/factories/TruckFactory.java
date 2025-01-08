678acafe743b2e1e671c74b567712db0402d895c
mrsxnshxne <jean.gabriel.trousseau12@gmail.com>
2025-01-08 15:35:24 +0100

    docs(factory): adding Javadoc in the TruckFactory interface

--- src/main/java/fr/mrsunshine/factories/TruckFactory.java
+++ src/main/java/fr/mrsunshine/factories/TruckFactory.java
@@ -7,7 +7,22 @@ import java.util.List;
 
 
 public interface TruckFactory {
+    /**
+     * This function will create, using the CLI and user arguments, a trunk
+     * @return Truck object
+     */
     Truck createTruck();
+
+
+    /**
+     * This function remove a truck
+     * @param truckHandler A TruckHandler object
+     */
     void removeTruck(TruckHandler truckHandler);
+
+
+    /**
+     * @param trucks A list of Trucks
+     */
     void showTrucks(List<Truck> trucks);
 }
